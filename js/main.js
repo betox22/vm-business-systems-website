71f7e08f96ca838e53f7fed34f5b519eb88a7886
Alberto <newappcreathor@email.com>
2026-05-08 23:17:42 -0400

    Initial V&M Business Systems public website

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const navToggle = document.getElementById("navToggle");
+const navMenu = document.getElementById("navMenu");
+
+if (navToggle && navMenu) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.toggle("show");
+  });
+}
